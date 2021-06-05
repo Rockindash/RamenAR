0457c119d142cfc4f999abaa22d3dc8fee7151e8
Amol Kumar <amolkmr10@gmail.com>
2021-06-05 16:44:06 -0400

    Initial Commit

---
+++ Ramen 3/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Ramen 3
+//
+//  Created by Amol Kumar on 2021-06-05.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}

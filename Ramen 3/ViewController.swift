d72def55f147a3860caaac3ead82ea1bc6730d7d
Amol Kumar <amolkmr10@gmail.com>
2021-06-05 16:53:03 -0400

    Project init

--- Ramen 3/ViewController.swift
+++ Ramen 3/ViewController.swift
@@ -15,10 +15,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let box = createBox()
+        placeBox(box: box)
+        installGesture(on: box)
+    }
+    
+    func createBox() -> ModelEntity {
+        let box = MeshResource.generateBox(size: 0.5)
+        let boxMaterial = SimpleMaterial(color: .blue, isMetallic: true)
+        let boxEntity = ModelEntity(mesh: box, materials: [boxMaterial])
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        return boxEntity
+    }
+    
+    func placeBox(box: ModelEntity) {
+        let boxAnchor = AnchorEntity(world: SIMD3(x: 0, y: 0, z: 0))
+        boxAnchor.addChild(box)
+        
+        arView.scene.addAnchor(boxAnchor)
+    }
+    
+    func installGesture(on object: ModelEntity) {
+        object.generateCollisionShapes(recursive: true)
+        arView.installGestures([.translation, .scale], for: object)
     }
 }
